27ae0dd67a378ef882495d27d6c0b5882f678166
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-07-20 23:38:38 -0300

    Create Soma de dois numero.kt

---
+++ Soma de dois numero.kt
@@ -0,0 +1,10 @@
+import java.util.*
+ 
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    println("ola");
+    val numeroUm= 10;
+    val numeroDois= 140
+    val soma= numeroUm + numeroDois;
+    println(soma);
+}
